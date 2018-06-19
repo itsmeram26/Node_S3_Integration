b843cf8a79fce028862b10fe9f9e075504572919
Ramkumar Seshadri <itsmeram14@gmail.com>
2018-06-19 16:51:52 -0400

    Moved the require under the appropriate comments

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const listFilesS3 = require('./src/service/ListFiles');
 const createFilesS3 = require('./src/service/CreateFiles');
-const deleteFileS3 = require('./src/service/DeleteFile');
-
 createFilesS3.uploadFileInS3();
 
 /* To list Objects in AWS S3
+const listFilesS3 = require('./src/service/ListFiles');
 listFilesS3.listS3Objects(); */
 
 /* To delete a file from AWS S3
+const deleteFileS3 = require('./src/service/DeleteFile');
 deleteFileS3.deleteObjectFromS3(); */
 
 
